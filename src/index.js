4a4edcc1d7adc09209ca94ff51e45fff8e8eac71
metricaez <emiliano@parity.io>
2023-07-03 11:17:21 -0300

    basic local host query test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// Required imports
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+
+// Initialise the provider to connect to the local node
+const provider = new WsProvider('ws://127.0.0.1:9944');
+
+// Create the API and wait until ready
+const api = await ApiPromise.create({ provider });
+
+await api.isReady;
+
+// Retrieve the chain & node information information via rpc calls
+const [chain, nodeName, nodeVersion] = await Promise.all([
+  api.rpc.system.chain(),
+  api.rpc.system.name(),
+  api.rpc.system.version()
+]);
+
+console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
